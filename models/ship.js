b094f3b9a9ef51c151d67b430d2b9511d0a20524
Taylor <trflagg@gmail.com>
2013-08-16 22:32:30 -0400

    Make Ship into a registered Db model.

--- models/ship.js
+++ models/ship.js
@@ -1,33 +1,46 @@
 module.exports = function(db) {
 
     var util = require('util'),
-        Avatar = require('../argie/models/avatar')(db, 'ship'),
+        Avatar = require('../argie/models/avatar')(db, 'Ship'),
         MessageHolder = require('../argie/models/messageHolder')(db);
 
-    Ship = function() {
-        this.super_.call(this);
+    Ship = function(doc) {
+        Ship.super_.call(this, doc);
 
-        // controls and crew members are child messageHolders
-        var crew = new MessageHolder();
-        crew.addChild('security', new MessageHolder());
-        crew.addChild('medical', new MessageHolder());
-        crew.addChild('empat', new MessageHolder());
-        crew.addChild('engineering', new MessageHolder());
-        crew.addChild('cultural', new MessageHolder());
-        crew.addChild('info', new MessageHolder());
-        this.addChild('crew', crew);
+        if (doc) {
 
-        var commands = new MessageHolder();
-        commands.addChild('weapons', new MessageHolder());
-        commands.addChild('shields', new MessageHolder());
-        commands.addChild('sensors', new MessageHolder());
-        commands.addChild('databank', new MessageHolder());
-        commands.addChild('processor', new MessageHolder());
-        this.addChild('commands', commands);
+        }
+        else {
+            // controls and crew members are child messageHolders
+            var crew = new MessageHolder();
+            crew.addChild('security', new MessageHolder());
+            crew.addChild('medical', new MessageHolder());
+            crew.addChild('empat', new MessageHolder());
+            crew.addChild('engineering', new MessageHolder());
+            crew.addChild('cultural', new MessageHolder());
+            crew.addChild('info', new MessageHolder());
+            this.addChild('crew', crew);
 
-        this.addChild('direct_messages', new MessageHolder());
+            var commands = new MessageHolder();
+            commands.addChild('weapons', new MessageHolder());
+            commands.addChild('shields', new MessageHolder());
+            commands.addChild('sensors', new MessageHolder());
+            commands.addChild('databank', new MessageHolder());
+            commands.addChild('processor', new MessageHolder());
+            this.addChild('commands', commands);
+
+            this.addChild('direct_messages', new MessageHolder());
+        }
     }
     util.inherits(Ship, Avatar);
+
+    Ship.prototype.onSave = function(ship) {
+        var doc = Ship.super_.prototype.onSave(ship);
+
+        return doc;
+    };
+
+    db.register('Ship', Ship);
 
     return Ship;
 }
