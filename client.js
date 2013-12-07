fb5cc503256d3532180aaa1be6407625015e967d
Taylor <trflagg@gmail.com>
2013-12-07 00:12:10 -0500

    argie added system.clearScreen()

--- client.js
+++ client.js
@@ -1,69 +1,84 @@
 
 var firstMessage = 'INIT';
 
-var Fiber = require('fibers');
-
-var environment = require('./environment-dev'),
+var environment = require('./environments/environment-local'),
     readline = require('readline'),
-    Db = require('./argie/argieDb/db');
+    Db = require('./argie/argieDb/db'),
+    constants = require('./argie/constants');
 
-function sleep(ms) {
-    var fiber = Fiber.current;
-    setTimeout(function() {
-        fiber.run();
-    }, ms);
-    Fiber.yield();
+
+// the worst thing ever to do in nodejs EVER
+function sleep(miliseconds) {
+    var start = new Date;
+    
+    // NEVER DO THIS!  I AM A PROFESSIONAL!
+    while((new Date - start) <= miliseconds) {
+        ;
+    }
+}
+
+function printLines(result, callback) {
+
+    if (result) {
+        var lines = result.split('\n');
+
+        for (var i = 0, ll = lines.length; i<ll; i++) {
+            var currentLine = lines[i];
+
+            // check for wait string
+            // console.log(constants.waitRegEx.exec(currentLine));
+            if ((regExArray = constants.waitRegEx.exec(currentLine)) != null) {
+                // wait in the worst way possible
+                sleep(regExArray[1])
+            }
+            // check for clearScreen string
+            else if ((regExArray = constants.clearScreenRegEx.exec(currentLine)) != null) {
+                clearScreen();
+            }
+            else {
+                console.log(currentLine);
+            }
+        }
+    }
 }
 
 function start() {
-
-    // ship is global
-    ship = db.create('Ship');
+    // avatar is global
+    avatar = initAvatar();
 
     db.load('Message', {name: firstMessage}, function(err, message) {
         if (err) {
             console.log("ERROR in start:" + err);
         }
-        // end();
-        doLoop(message);
+        else {
+            message.run(avatar, function(err, result) {
+                
+                // show result of message
+                printLines(result);
+
+                //show command options
+                promptOptions(avatar.getCommandTextList(), '');
+            });
+        }
     })
 }
 
+function initAvatar() {
 
-function doLoop(message) {
-    if (message) {
-        var result = message.run(ship, function(err, result) {
-            
-            // show result of message
-            console.log(result);
+    Ship = require('./models/Ship')(db);
 
-            //gOptions is global
-            gOptions = ship.getCommandTextList();
+    var ship = db.create('Ship');
+    ship.setGlobal('name', 'Picard');
 
-            //show command options
-            promptOptions('');
-        });
-    }
+    return ship;
 }
 
-var printLines = Fiber(function(str) {
-    lines = str.split('\n');
-    for (i=0, ll=lines.length; i<ll; i++) {
-        console.log(lines[i]);
-        sleep(500);
-    }
-});
 
 function clearScreen() {
     console.log("\033[2J\033[0f");
 }
 
-function promptOptions(currentChoice) {
-    var options = gOptions;
-    // console.log(options);
-    // console.log(currentChoice);
-                    // console.log(ship.child('direct_messages'));
-
+function promptOptions(options, currentChoice) {
     var currentOptions = options;
     if (currentChoice != '') {
         var choices = currentChoice.split('.');
@@ -71,22 +86,28 @@ function promptOptions(currentChoice) {
 
         for (var i=0, ll=choices.length; i<ll; i++) {
 
-            if (currentOptions[choices[i]].children) {
-                currentOptions = currentOptions[choices[i]].children;
-                // console.log(currentOptions);
+            if (currentOptions[choices[i]]) {
+                if (currentOptions[choices[i]].children) {
+                    currentOptions = currentOptions[choices[i]].children;
+                    // console.log(currentOptions);
+                }
+                else {
+                    var childString = makeChildString(options, choices)
+                    // console.log('cs'+childString);
+                    var result = avatar.runMessage(currentOptions[choices[i]].text, childString, function(err, result) {
+                        if (err) {
+                            console.log(err);
+                        }
+                        else {
+                            printLines(result);
+                        }
+                        promptOptions(avatar.getCommandTextList(), '');
+                    });
+                    return;
+                }
             }
             else {
-                var childString = makeChildString(options, choices)
-                // console.log('cs'+childString);
-                var result = ship.runMessage(currentOptions[choices[i]].text, childString, function(err, result) {
-                    if (err) {
-                        console.log(err);
-                    }
-                    clearScreen();
-                    console.log(result);
-                    gOptions = ship.getCommandTextList();
-                    promptOptions('');
-                });
+                promptOptions(avatar.getCommandTextList(), '');
                 return;
             }
         }
@@ -111,36 +132,36 @@ function promptOptions(currentChoice) {
 
     //poll if necessary
     // console.log(ship.getGlobal('yield'));
-    if (ship.getGlobal('yield') === 1) {
+    if (avatar.getGlobal('yield') === 1) {
         pollForYield();
     }
+
     rl.question('>', function(answer) {
         console.log('====================================');
         console.log('');
         if (answer === 'b') {
-            promptOptions(currentChoice.split('.').slice(0,-1).join('.'));
+            promptOptions(options, currentChoice.split('.').slice(0,-1).join('.'));
         }
         else if (!handleOptions(answer)) {
             console.log('');
             currentChoice = addChoice(currentChoice, answer);
 
-            promptOptions(currentChoice);
+            promptOptions(options, currentChoice);
         }
     });
 }
 
 function pollForYield() {
     setTimeout(function() {
-        if (ship.getGlobal('yield') === 1) {
-            ship.pollForYield(function(err, result) {
+        if (avatar.getGlobal('yield') === 1) {
+            avatar.pollForYield(function(err, result) {
                 if (err) {
                     console.log(err);
                 }
                 if (result != false) {
                     console.log('');
                     console.log(result);
-                    gOptions = ship.getCommandTextList();
-                    promptOptions('');
+                    promptOptions(avatar.getCommandTextList(), '');
                 }
                 else {
                     pollForYield();
@@ -212,6 +233,11 @@ function end() {
     rl.close();
 }
 
+// for subclasses to override with their own init stuff
+function init() {
+
+}
+
 module.exports = function() {
 
     // db and rl are global
@@ -219,9 +245,10 @@ module.exports = function() {
     rl = readline.createInterface(process.stdin, process.stdout);
     rl.setPrompt('>');
 
-    require('./models')(db);
+    Avatar = require('./argie/models/avatar')(db),
+    Message = require('./argie/models/message')(db);
 
-    console.log('Welcome to MiS!');
+    init();
 
     start();
 }();
