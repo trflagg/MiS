22465fc2fc74b2d6c497c6bdf2e44f09110fe3a2
Taylor <trflagg@gmail.com>
2014-05-25 22:38:20 -0400

    Add outputView and outputModel

---
+++ public/js/game/outputModel.js
@@ -0,0 +1,11 @@
+define([ 
+    'backbone'
+], function(Backbone) {
+
+    var outputModel = Backbone.Model.extend({
+        initialize: function() {
+        },
+    });
+
+    return outputModel;
+});
---
+++ public/js/game/outputView.js
@@ -0,0 +1,22 @@
+define([ 
+    'backbone',
+    'doT!game/templates/outputView'
+], function(Backbone, template) {
+
+    var outputView = Backbone.View.extend({
+        initialize: function() {
+            this.template = template;
+
+            if (this.model) {
+                this.listenTo(this.model, 'change', this.render);
+            }
+        },
+
+        render: function() {
+            $(this.el).html( this.model.get("text") );
+            return this; 
+        }
+    });
+
+    return outputView;
+});
