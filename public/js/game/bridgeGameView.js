4d2ba99fed8c8aa3c5531c6967de9c2445c9a78d
Taylor <trflagg@gmail.com>
2014-06-18 20:29:46 -0400

    appView moved to bridgeGameView and replaced with empty verision

---
+++ public/js/game/bridgeGameView.js
@@ -0,0 +1,66 @@
+define([ 
+    'backbone',
+    'game/shipModel',
+    'game/locationView',
+    'game/infoView',
+    'game/infoModel',
+    'game/commandHolderView',
+    'game/commandHolderViewTopLevel',
+    'game/outputView',
+    'game/outputModel'
+], function(Backbone, ShipModel, LocationView, InfoView, InfoModel, CommandHolderView, CommandHolderViewTopLevel, OutputView, OutputModel) {
+
+    var appView = Backbone.View.extend({
+        initialize: function() {
+            this.ship = new ShipModel({
+                id: $("#ship-data").data('id')
+            });
+            this.listenTo(this.ship, 'change', this.shipChanged);
+
+            this.locationView = new LocationView({
+                model: this.ship.get("location"),
+                el: $("#location")
+            });
+
+            this.info = new InfoModel();
+            this.infoView = new InfoView({
+                model: this.info,
+                el: $("#info")
+            });
+
+            this.output = new OutputModel();
+            this.outputView = new OutputView({
+                model: this.output,
+                el: $("#output")
+            });
+
+            this.crewView = new CommandHolderViewTopLevel({
+                model: this.ship.get("crew"),
+                el: $("#crew")
+            });
+
+            this.shipControlsView = new CommandHolderViewTopLevel({
+                model: this.ship.get("shipControls"),
+                el: $("#shipControls")
+            });
+
+            this.directMessagesView = new CommandHolderView({
+                model: this.ship.get("directMessages"),
+                el: $("#directMessages")
+            });
+        },
+    });
+
+    appView.prototype.shipChanged = function() {
+        var ship = this.ship;
+
+        this.info.set({
+            captain: ship.get("captain"),
+            shipName: ship.get("shipName")
+        });
+
+        this.output.set("text", ship.get("output"));
+    };
+
+    return appView;
+});
