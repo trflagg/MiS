5c75f1665092de57c2ef503a15a6785f9b306743
Taylor Flagg <trflagg@gmail.com>
2014-07-03 14:20:15 -0400

    Type out prologue title text.

--- public/js/game/titleGameView.js
+++ public/js/game/titleGameView.js
@@ -10,15 +10,35 @@ define([
 
         render: function() {
             $(this.el).html(this.template());
+            $("#titleGameView__title").hide();
+            $("#titleGameView__text").hide();
             return this; 
         }
 
     });
 
     titleView.prototype.fadeTitle = function() {
-        this.$el.hide();
         this.render();
-        this.$el.fadeIn({
+        var title = $("#titleGameView__title");
+        var thisView = this;
+
+        title.fadeIn({
+            duration: 2000,
+            easing: 'linear'
+        }).delay(2000).fadeOut({
+            duration: 2000,
+            easing: 'linear',
+            complete: function() {
+                thisView.typeText("Prologue");
+            }
+        });
+    };
+
+    titleView.prototype.fadeText = function(text) {
+        var text_el = $("#titleGameView__text");
+        text_el.text(text);
+
+        text_el.fadeIn({
             duration: 2000,
             easing: 'linear'
         }).delay(2000).fadeOut({
@@ -26,5 +46,18 @@ define([
             easing: 'linear'
         });
     };
+
+    titleView.prototype.typeText = function(text) {
+        $("#titleGameView__text").show();
+        showText("#titleGameView__text", text, 0, 50);
+    };
+
+    var showText = function (target, message, index, interval) {
+        if (index < message.length) { 
+            $(target).append(message[index++]); 
+            setTimeout(function () { showText(target, message, index, interval); }, interval); 
+        } 
+    }
+    
     return titleView;
 });
